685f5265a84a14f56796b57366b5b0d88993e037
Renato Lira <renato.lira.14@gmail.com>
2022-09-26 21:49:21 -0300

    Now using commit status and adding csv output file logic

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 from github import Github
 import re
+import csv
 
-g = Github("<YOUR TOKEN HERE>")
+g = Github("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")
 
-regex_pattern = "(Add|added|adding)[a-zA-Z ]*(feature(-)?( )?(flag|toogle)?)"
+add_ff_regex_pattern = "(?i)(add|added|adding)[a-zA-Z ]*(feature(-)?( )?(flag|toggle|flipper|switch))"
+remove_ff_regex_pattern = "(?i)(remove|removing|removed|delete|deleting|deleted)[a-zA-Z ]*(feature(-)?( )?(flag|toggle|flipper|switch))"
+
+f = open('output.csv', 'w')
+writer = csv.writer(f)
+header = ['REPOSITORY', 'FF COMMITS', 'SUCCESSFUL', 'FAILED/ERROR', 'PENDING']
+writer.writerow(header)
 
 with open("repos.txt", "r") as repos_file:
-  for line in repos_file:
-    repo_path = line.strip()
-    repo = g.get_repo(repo_path)
-    print("Repo -> ", repo.name)
-    commits = repo.get_commits()
-    for commit in commits:
-        result = re.match(regex_pattern, commit.commit.message)
-        if not result == None :
-            print(commit.commit.message)
-            check_runs = commit.get_check_runs()
-            for check_run in check_runs:
-                print(check_run.status)
-                print(check_run.conclusion, "\n")
 
+    for line in repos_file:
+        repo_path = line.strip()
+        repo = g.get_repo(repo_path)
+        print("Repository:", repo.name)
+        commits = repo.get_commits()
+        commits_adding_ff = 0
+        successful_commits = 0
+        failed_commits = 0
+        pending_commits = 0
+        print("Iterating {} commits".format(commits.totalCount))
+        for commit in commits:
+            adding_match = re.match(
+                add_ff_regex_pattern, commit.commit.message)
+            if not adding_match == None:
+                commits_adding_ff += 1
+                print(commit.commit.message)
+                print("SHA:", commit.sha)
+                print("commit_status:", commit.get_combined_status().state)
+                commit_state = commit.get_combined_status().state
+                if commit_state == 'success':
+                    successful_commits += 1
+                elif commit_state == 'pending':
+                    pending_commits += 1
+                else:
+                    failed_commits += 1
 
+        print("FF_COMMITS: {} -------- SUCCESSFUL: {} -------- FAILED/ERROR: {} -------- PENDING: {}".format(
+            commits_adding_ff, successful_commits, failed_commits, pending_commits))
 
+        data = [repo_path, commits_adding_ff,
+                successful_commits, failed_commits, pending_commits]
+        writer.writerow(data)
 
-
+f.close()
